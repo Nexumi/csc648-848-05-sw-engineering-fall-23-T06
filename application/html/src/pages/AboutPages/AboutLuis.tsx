36836247a3ef396afa35fa7fb98c63d1bfa65caa
MannyR00 <mannyramirez1330@gmail.com>
2023-09-12 15:46:32 -0700

    added a picture for my profile picture and added a description as well - Luis

--- application/html/src/pages/AboutPages/AboutLuis.tsx
+++ application/html/src/pages/AboutPages/AboutLuis.tsx
@@ -1,9 +1,12 @@
 import AboutMe from "../../components/AboutMe";
+import Luis from "../../assets/profiles/Luis.png"
 
 export default function AboutLuis() {
   return (
     <AboutMe
+      img={Luis}
       name="Luis"
+      description="Hello, my name is Luis I'm a computer science major and in my free time, I enjoy listening to music or playing games."
     />
   );
 }
