4c10a95b98129656501cdf8a9a4b946e29730377
liendavid <dlien@mail.sfsu.edu>
2023-09-12 10:39:56 -0700

    updated David's about page

--- application/html/src/pages/AboutPages/AboutDavid.tsx
+++ application/html/src/pages/AboutPages/AboutDavid.tsx
@@ -1,9 +1,12 @@
 import AboutMe from "../../components/AboutMe";
+import David from "../../assets/profiles/David.jpeg"
 
 export default function AboutDavid() {
   return (
     <AboutMe
-      name="David"
+      img={David}
+      name="David Lien"
+      description="Hello my name is David. I am a senior at SFSU who is about to graduate. I recently got into pickleball and I find myself at the courts at least twice a week."
     />
   );
 }
