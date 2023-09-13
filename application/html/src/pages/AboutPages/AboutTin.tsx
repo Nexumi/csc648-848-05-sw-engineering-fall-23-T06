39da2a2fe9bfc375bec7664c117d5050ed908a9d
Tin Nguyen <tastyviet@gmail.com>
2023-09-12 21:44:07 -0700

    Added in my 'about me' description.no image yet

--- application/html/src/pages/AboutPages/AboutTin.tsx
+++ application/html/src/pages/AboutPages/AboutTin.tsx
@@ -3,7 +3,12 @@ import AboutMe from "../../components/AboutMe";
 export default function AboutTin() {
   return (
     <AboutMe
-      name="Tin"
+        //img={}
+        name="Tin Nguyen"
+        description="My name is Jimmy Pan, and I am majoring in computer science
+    at San Francisco State University. I enjoy spending time with my friends
+    and playing video games."
     />
   );
+
 }
