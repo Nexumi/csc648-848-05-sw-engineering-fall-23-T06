c71333e9663a06bf795fb6281fca3aad13b26eb9
mayeung <mankityeung15@gmail.com>
2023-09-12 18:39:41 -0700

    AboutMankit:update pic and description

--- application/html/src/pages/AboutPages/AboutMankit.tsx
+++ application/html/src/pages/AboutPages/AboutMankit.tsx
@@ -1,9 +1,14 @@
 import AboutMe from "../../components/AboutMe";
+import Mankit from "../../assets/profiles/Mankit.jpg"
 
 export default function AboutMankit() {
   return (
     <AboutMe
-      name="Mankit"
+      img={Mankit}
+      name="Mankit Yeung"
+      description="Hi, my name is Mankit. I am a computer science major and 
+      during my free time I like to read some fiction books and play some 
+      video games."
     />
   );
 }
