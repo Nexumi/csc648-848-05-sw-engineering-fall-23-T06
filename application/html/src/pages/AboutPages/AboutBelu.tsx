27cb0f602ce33bfb1c61a4e152ad99b2175d8bb6
ritabeluvelazco <77955704+ritabeluvelazco@users.noreply.github.com>
2023-09-16 23:51:38 -0700

    Belu's About Me Page was updated with description, name, and photo. Added Belu's image in profiles folder. Done by Belu V.

--- application/html/src/pages/AboutPages/AboutBelu.tsx
+++ application/html/src/pages/AboutPages/AboutBelu.tsx
@@ -1,9 +1,16 @@
 import AboutMe from "../../components/AboutMe";
+import Belu from "../../assets/profiles/Belu.jpg";
 
 export default function AboutBelu() {
   return (
     <AboutMe
-      name="Belu"
+      img={Belu}
+      name="Rita Belu Velazco"
+      description="Hello! My name is Rita Belu Velazco, but I go by Belu. 
+      My pronouns are she/her and I am a senior majoring in computer 
+      science with a minor in mathematics at San Francisco State University. 
+      I'm the team lead of Team 6 and some personal interests of mine are 
+      journaling and fashion."
     />
   );
 }
