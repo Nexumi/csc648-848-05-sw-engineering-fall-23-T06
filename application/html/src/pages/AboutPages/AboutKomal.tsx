62bdb05106cd733f86a9cfe197a2e264c369b326
Komal Kaur <kkaur5@mail.sfsu.edu>
2023-09-19 21:59:50 -0700

    AboutMe Komal

--- application/html/src/pages/AboutPages/AboutKomal.tsx
+++ application/html/src/pages/AboutPages/AboutKomal.tsx
@@ -1,9 +1,14 @@
 import AboutMe from "../../components/AboutMe";
+import Belu from "../../assets/profiles/Komal.jpg";
 
 export default function AboutKomal() {
   return (
     <AboutMe
-      name="Komal"
+      name="Komaldeep Kaur"
+      description="Hi everyone! My name is Komaldeep Kaur but, I go by Komal.
+      I am a computer science major at San Fransisco State. I'm in my last year
+      and my focus is artifcial intelligence. I enjoy going to the gym to promote
+      my healthy lifestyle in my free time."
     />
   );
 }
