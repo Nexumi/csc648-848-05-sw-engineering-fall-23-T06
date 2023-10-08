e6ebe5495ebf92b59d69d27fb1287148ae17b6f8
Jimmy Pan <jpan6@mail.sfsu.edu>
2023-10-08 01:19:44 -0700

    PageNotFound: center text

--- application/html/src/pages/ForgetPage.tsx
+++ application/html/src/pages/ForgetPage.tsx
@@ -4,7 +4,7 @@ import { Flex } from "../common/layout/flex";
 export default function ForgetPage() {
   return (
     <>
-      <Flex justifyContent="center" class="h-full">
+      <Flex justifyContent="center" class="h-full text-center">
         <div>
           <p>(WIP) Page not yet constructed</p>
           <div class="underline">
--- application/html/src/pages/PageNotFound.tsx
+++ application/html/src/pages/PageNotFound.tsx
@@ -4,7 +4,7 @@ import { Flex } from "../common/layout/flex";
 export default function PageNotFound() {
   return (
     <>
-      <Flex justifyContent="center" class="h-full">
+      <Flex justifyContent="center" class="h-full text-center">
         <div>
           <p>404 Page Not Found</p>
           <div class="underline">
