aea71d8c65f90e07cdcfc1fb86a147822af3f68a
Jimmy Pan <jpan6@mail.sfsu.edu>
2023-10-08 01:10:09 -0700

    PageNotFound: created

--- application/html/src/Root.tsx
+++ application/html/src/Root.tsx
@@ -9,24 +9,28 @@ import AboutKomal from "./pages/AboutPages/AboutKomal";
 import AboutLuis from "./pages/AboutPages/AboutLuis";
 import AboutMankit from "./pages/AboutPages/AboutMankit";
 import AboutTin from "./pages/AboutPages/AboutTin";
+import LoginPage from "./pages/LoginPage";
+import PageNotFound from "./pages/PageNotFound";
 
 export default function Root() {
   return (
     <div class="h-screen">
-      <div class="px-10 py-8">
-        <Header />
+      <Header />
+      <div class="h-full px-10 py-8">
         <Routes>
-          <Route path="/" component={HomePage} />
-          <Route path="/about">
-            <Route path="/" component={AboutPage} />
-            <Route path="/belu" component={AboutBelu} />
-            <Route path="/david" component={AboutDavid} />
-            <Route path="/jimmy" component={AboutJimmy} />
-            <Route path="/komal" component={AboutKomal} />
-            <Route path="/luis" component={AboutLuis} />
-            <Route path="/mankit" component={AboutMankit} />
-            <Route path="/tin" component={AboutTin} />
-          </Route>
+            <Route path="/" component={HomePage} />
+            <Route path="/about">
+              <Route path="/" component={AboutPage} />
+              <Route path="/belu" component={AboutBelu} />
+              <Route path="/david" component={AboutDavid} />
+              <Route path="/jimmy" component={AboutJimmy} />
+              <Route path="/komal" component={AboutKomal} />
+              <Route path="/luis" component={AboutLuis} />
+              <Route path="/mankit" component={AboutMankit} />
+              <Route path="/tin" component={AboutTin} />
+            </Route>
+          <Route path="/login" component={LoginPage} />
+          <Route path="/*" component={PageNotFound} />
         </Routes>
       </div>
     </div>
---
+++ application/html/src/pages/PageNotFound.tsx
@@ -0,0 +1,21 @@
+import { A } from "@solidjs/router";
+import { Flex } from "../common/layout/flex";
+
+export default function PageNotFound() {
+  return (
+    <>
+      <Flex justifyContent="center" class="h-full">
+        <div>
+          <p>404 Page Not Found</p>
+          <div class="underline">
+            <A
+              href="/"
+            >
+              Return to Home Page
+            </A>
+          </div>
+        </div>
+      </Flex>
+    </>
+  );
+}
