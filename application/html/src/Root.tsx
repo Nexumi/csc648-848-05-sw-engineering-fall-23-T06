5675985748fec28e670285c8b0a68ee3ad0ab4d8
Jimmy Pan <jpan6@mail.sfsu.edu>
2023-10-08 01:14:53 -0700

    ForgetPage: create file

--- application/html/src/Root.tsx
+++ application/html/src/Root.tsx
@@ -10,6 +10,7 @@ import AboutLuis from "./pages/AboutPages/AboutLuis";
 import AboutMankit from "./pages/AboutPages/AboutMankit";
 import AboutTin from "./pages/AboutPages/AboutTin";
 import LoginPage from "./pages/LoginPage";
+import ForgetPage from "./pages/ForgetPage";
 import PageNotFound from "./pages/PageNotFound";
 
 export default function Root() {
@@ -30,6 +31,7 @@ export default function Root() {
               <Route path="/tin" component={AboutTin} />
             </Route>
           <Route path="/login" component={LoginPage} />
+          <Route path="/forget" component={ForgetPage} />
           <Route path="/*" component={PageNotFound} />
         </Routes>
       </div>
---
+++ application/html/src/pages/ForgetPage.tsx
@@ -0,0 +1,21 @@
+import { A } from "@solidjs/router";
+import { Flex } from "../common/layout/flex";
+
+export default function ForgetPage() {
+  return (
+    <>
+      <Flex justifyContent="center" class="h-full">
+        <div>
+          <p>(WIP) Page not yet constructed</p>
+          <div class="underline">
+            <A
+              href="/"
+            >
+              Return to Home Page
+            </A>
+          </div>
+        </div>
+      </Flex>
+    </>
+  );
+}
