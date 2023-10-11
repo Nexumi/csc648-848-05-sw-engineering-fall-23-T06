35526a43c73885993dd471d7d2775851961ae8e8
liendavid <dlien@mail.sfsu.edu>
2023-10-11 15:01:27 -0700

    working on order and items

--- application/backend/src/main/java/item/ItemRepository.java
+++ application/backend/src/main/java/item/ItemRepository.java
@@ -1,8 +1,8 @@
-package order;
+package item;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
+public interface ItemRepository extends JpaRepository<Item,Long> {
 }
--- application/backend/src/main/java/item/ItemService.java
+++ application/backend/src/main/java/item/ItemService.java
@@ -1,4 +1,22 @@
 package item;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Autowired
+    public ItemService(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
+    public List<Item> getItems(){
+        return itemRepository.findAll();
+    }
 }
