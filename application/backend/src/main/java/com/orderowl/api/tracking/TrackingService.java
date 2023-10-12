a7ba7779028dfe86a60aef290d678790377f8354
Tin Nguyen <tastyviet@gmail.com>
2023-10-12 14:16:47 -0700

    small line fix

--- application/backend/src/main/java/com/orderowl/api/tracking/TrackingService.java
+++ application/backend/src/main/java/com/orderowl/api/tracking/TrackingService.java
@@ -1,4 +1,4 @@
-package com.orderowl.api.tacking;
+package com.orderowl.api.tracking;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
