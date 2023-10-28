1bdfd219cdd8558b5ff67910a96f31fc92e02789
liendavid <dlien@mail.sfsu.edu>
2023-10-27 17:46:45 -0700

    removed unused code

--- application/backend/src/main/java/com/orderowl/api/test/TestEntityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.orderowl.api.test;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestEntityRepository extends JpaRepository<TestEntity, Long> {
-    // You can add custom query methods here if needed
-}
