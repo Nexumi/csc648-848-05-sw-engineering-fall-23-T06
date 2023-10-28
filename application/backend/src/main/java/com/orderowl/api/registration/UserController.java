3a0e7a8a088273014f290f0822a0868be4600d13
liendavid <dlien@mail.sfsu.edu>
2023-10-27 18:17:24 -0700

    removed unsed import

--- application/backend/src/main/java/com/orderowl/api/registration/UserController.java
+++ application/backend/src/main/java/com/orderowl/api/registration/UserController.java
@@ -1,7 +1,6 @@
 package com.orderowl.api.registration;
 
 
-import com.orderowl.api.tracking.TrackingEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
