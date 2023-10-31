c7dd19a556e1631fdedef19701150e10f813309f
Jimmy Pan <jpan6@mail.sfsu.edu>
2023-10-30 23:35:31 -0700

    Header: changed settings icon + dropdown menu

--- application/html/src/components/Header.tsx
+++ application/html/src/components/Header.tsx
@@ -6,6 +6,7 @@ import { uriAbout, uriForget, uriHome, uriLogin, uriRegistration } from "../util
 import toast from "solid-toast";
 import Cookies from "js-cookie";
 import { Button } from "../common/components/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../common/components/dropdown-menu";
 
 
 export default function Logo() {
@@ -48,10 +49,8 @@ export default function Logo() {
                 />
               </svg>
             </div>
-            <div
-              class="cursor-pointer"
-              onClick={handleSystemClick}
-            >
+            <DropdownMenu>
+              <DropdownMenuTrigger>
               <svg
                 fill="none"
                 viewBox="0 0 24 24"
@@ -59,17 +58,33 @@ export default function Logo() {
                 stroke="currentColor"
                 class="w-8 h-8"
               >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 011.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.56.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.893.149c-.425.07-.765.383-.93.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 01-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.397.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 01-.12-1.45l.527-.737c.25-.35.273-.806.108-1.204-.165-.397-.505-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.107-1.204l-.527-.738a1.125 1.125 0 01.12-1.45l.773-.773a1.125 1.125 0 011.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894z"
-                />
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                />
+                <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
               </svg>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuItem
+                  onSelect={() => {
+                    toast.error("Not yet implemented");
+                  }}
+                  class="cursor-pointer hover:bg-gray-200"
+                >
+                  Settings
+                </DropdownMenuItem>
+                <DropdownMenuItem
+                  onSelect={() => {
+                    toast.success(`See you next time ${Cookies.get("user")}!`);
+                    Cookies.remove("user");
+                  }}
+                  class="cursor-pointer hover:bg-gray-200"
+                >
+                  Logout
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+            {/* <div
+              class="cursor-pointer"
+              onClick={handleSystemClick}
+            >
               <Show when={dropdownVisible()}>
                 <div class="absolute right-0 mt-2 w-48 bg-white border border-gray-200 rounded shadow-lg">
                   <div class="py-1">
@@ -86,7 +101,7 @@ export default function Logo() {
                   </div>
                 </div>
               </Show>
-            </div>
+            </div> */}
           </Flex>
         </header>
       </Show>
