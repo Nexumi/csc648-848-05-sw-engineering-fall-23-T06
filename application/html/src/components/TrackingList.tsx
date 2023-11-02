adf6aaef7e1997e62aa0ecf002b0fefcb09859cf
Jimmy Pan <jpan6@mail.sfsu.edu>
2023-11-02 00:21:49 -0700

    TrackingList: sorting

--- application/html/src/components/TrackingList.tsx
+++ application/html/src/components/TrackingList.tsx
@@ -1,42 +1,101 @@
-import { For } from "solid-js";
+import { For, Match, Switch } from "solid-js";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "../common/components/table";
-import { useNavigate } from "@solidjs/router";
+import { useNavigate, useSearchParams } from "@solidjs/router";
 import { uriTracking } from "../utils/uri";
+import { sortBy } from "../utils/util";
+import { Flex } from "../common/layout/flex";
 
 export default function TrackingList(props: {
   display: any
   limit?: number
 }) {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  function updateSort(key: string) {
+    if (searchParams.key === key) {
+      const direction = Number(searchParams.direction) + 1;
+      if (direction === 2) {
+        setSearchParams({key: null, direction: null});
+      } else {
+        setSearchParams({direction: direction});
+      }
+    } else {
+      setSearchParams({key: key, direction: 0});
+    }
+  }
+
   return (
     <>
       <Table>
         <TableHeader>
           <TableRow>
             <TableHead>
-              Retailer
+              <HeaderTitle
+                title="Retailer"
+                key="retailer"
+                current={searchParams.key === "retailer"}
+                direction={searchParams.direction}
+                update={updateSort}
+              />
             </TableHead>
             <TableHead>
-              Carrier
+              <HeaderTitle
+                title="Carrier"
+                key="carrier"
+                current={searchParams.key === "carrier"}
+                direction={searchParams.direction}
+                update={updateSort}
+              />
             </TableHead>
             <TableHead>
-              ETA
+              <HeaderTitle
+                title="ETA"
+                key="eta"
+                current={searchParams.key === "eta"}
+                direction={searchParams.direction}
+                update={updateSort}
+              />
             </TableHead>
             <TableHead>
-              Status
+              <HeaderTitle
+                title="Status"
+                key="status"
+                current={searchParams.key === "status"}
+                direction={searchParams.direction}
+                update={updateSort}
+              />
             </TableHead>
             <TableHead>
-              Location
+              <HeaderTitle
+                title="Location"
+                key="location"
+                current={searchParams.key === "location"}
+                direction={searchParams.direction}
+                update={updateSort}
+              />
             </TableHead>
             <TableHead>
-              Address
+              <HeaderTitle
+                title="Address"
+                key="address"
+                current={searchParams.key === "address"}
+                direction={searchParams.direction}
+                update={updateSort}
+              />
             </TableHead>
             <TableHead>
-              Tracking Number
+              <HeaderTitle
+                title="Tracking Number"
+                key="trackingNumber"
+                current={searchParams.key === "trackingNumber"}
+                direction={searchParams.direction}
+                update={updateSort}
+              />
             </TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
-          <For each={props.limit ? props.display?.slice(0, props.limit) : props.display}>
+          <For each={sortBy((props.limit ? props.display?.slice(0, props.limit) : props.display), searchParams.key, searchParams.direction)}>
             {(pack) => 
               <TrackingRow
                 packageId={pack.id}
@@ -56,6 +115,63 @@ export default function TrackingList(props: {
   );
 }
 
+function HeaderTitle(props: {
+  title: string,
+  key: string,
+  current: boolean,
+  direction?: string | number,
+  update: Function
+}) {
+  return (
+    <>
+      <Flex
+        justifyContent="start"
+        class="cursor-pointer select-none gap-x-1"
+        onClick={() => {
+          props.update(props.key);
+        }}
+      >
+        {props.title}
+        <Switch>
+          <Match when={!props.current}>
+            <div class="w-2 h-2" />
+          </Match>
+          <Match when={Number(props.direction) === 0}>
+            <svg
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-2 h-2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M4.5 15.75l7.5-7.5 7.5 7.5"
+              />
+            </svg>
+          </Match>
+          <Match when={Number(props.direction) === 1}>
+            <svg
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-2 h-2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+              />
+            </svg>
+          </Match>
+        </Switch>
+      </Flex>
+    </>
+  );
+}
+
 function TrackingRow(props: {
   packageId?: string | number,
   retailer: string,
@@ -68,7 +184,7 @@ function TrackingRow(props: {
 }) {
   const navigate = useNavigate();
 
-  const HOVER = props.packageId !== undefined ? "hover:bg-gray-100" : "";
+  const HOVER = props.packageId !== undefined ? "hover:bg-gray-100 cursor-pointer" : "";
 
   return (
     <>
