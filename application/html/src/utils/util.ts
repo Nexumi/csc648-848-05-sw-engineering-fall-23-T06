c0b9b28dc204864fdef84e3d7c6411adf16837b6
Jimmy Pan <jpan6@mail.sfsu.edu>
2023-11-02 11:37:14 -0700

    util: added original website URL

--- application/html/src/utils/util.ts
+++ application/html/src/utils/util.ts
@@ -19,3 +19,20 @@ export function sortBy(objects: any, key: string, direction: string | number) {
 
   return direction == 0 ? sorted : sorted.reverse();
 }
+
+export function getURL(retailer: string) {
+  switch (retailer.toLowerCase()) {
+    case "amazon":
+      return "https://www.amazon.com/";
+    case "best buy":
+      return "https://www.bestbuy.com/";
+    case "ebay":
+      return "https://www.ebay.com/";
+    case "target":
+      return "https://www.target.com/";
+    case "walmart":
+      return "https://www.walmart.com/";
+    default:
+      return null;
+  }
+}
