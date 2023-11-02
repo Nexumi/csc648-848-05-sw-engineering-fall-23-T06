27adca3d2410c41210f90e5d7de378be6e8ab0d5
Jimmy Pan <jpan6@mail.sfsu.edu>
2023-11-02 11:55:59 -0700

    LoginPage & RegistrationPage: refactor submit button

--- application/html/src/pages/RegistrationPage.tsx
+++ application/html/src/pages/RegistrationPage.tsx
@@ -115,8 +115,6 @@ export default function RegistrationPage() {
                   </div>
                   <Button
                     type="submit"
-                    onMouseEnter={() => setIsHovered(true)}
-                    onMouseLeave={() => setIsHovered(false)}
                     class="relative overflow-visible"
                   >
                     <div
@@ -127,7 +125,7 @@ export default function RegistrationPage() {
                         viewBox="0 0 24 24"
                         stroke-width="1.5"
                         stroke="currentColor"
-                        class="w-6 h-6"
+                        class="w-8 h-8"
                       >
                         <path
                           stroke-linecap="round"
@@ -135,12 +133,10 @@ export default function RegistrationPage() {
                           d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 00-3.213-9.193 2.056 2.056 0 00-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 00-10.026 0 1.106 1.106 0 00-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12"
                         />
                       </svg>
-                      {isHovered() && 
-                        <span class="absolute top-1/2 transform -translate-y-1/2 ml-7 text-2x1">
-                          Submit
-                        </span>
-                      }
                     </div>
+                    <span class="ml-1 text-2x1">
+                      Submit
+                    </span>
                   </Button>
                 </Flex>
               </form>
