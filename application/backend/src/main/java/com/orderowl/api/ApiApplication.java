94b09d7e472e313263ade0446334ecc53c26f83a
liendavid <dlien@mail.sfsu.edu>
2023-11-17 01:01:51 -0800

    removed enable caching

--- application/backend/src/main/java/com/orderowl/api/ApiApplication.java
+++ application/backend/src/main/java/com/orderowl/api/ApiApplication.java
@@ -2,11 +2,8 @@ package com.orderowl.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class ApiApplication {
 
 	public static void main(String[] args) {
