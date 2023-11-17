64d97e3ec257011e16782495693d78de8ab6e054
liendavid <dlien@mail.sfsu.edu>
2023-11-17 01:04:04 -0800

    Added configuration to cross origin resource sharing(CORS) to allow frontend call backend APIs, commented out because I'm not sure if i want it in this file or SecurityConfiguration.java

--- application/backend/src/main/java/com/orderowl/api/CorsConfig.java
+++ application/backend/src/main/java/com/orderowl/api/CorsConfig.java
@@ -5,30 +5,32 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
 
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-
-        // Allow requests from this origin
-        config.addAllowedOrigin("https://alpha.orderowl.jpkit.us");
-        config.addAllowedOrigin("https://orderowl.jpkit.us");
-
-        // Allow all HTTP methods (GET, POST, etc.)
-        config.addAllowedMethod("*");
-
-        // Allow all headers
-        config.addAllowedHeader("*");
-
-        // Expose headers that the client can read
-        config.addExposedHeader("header1");
-        config.addExposedHeader("header2");
-
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        CorsConfiguration config = new CorsConfiguration();
+//
+//        // Allow requests from this origin
+//        config.addAllowedOrigin("https://alpha.orderowl.jpkit.us");
+//        config.addAllowedOrigin("https://orderowl.jpkit.us");
+//
+//        // Allow all HTTP methods (GET, POST, etc.)
+//        config.addAllowedMethod("*");
+//
+//        // Allow all headers
+//        config.addAllowedHeader("*");
+//
+//        // Expose headers that the client can read
+//        config.addExposedHeader("header1");
+//        config.addExposedHeader("header2");
+//
+//        source.registerCorsConfiguration("/**", config);
+//        return new CorsFilter(source);
+//    }
 }
