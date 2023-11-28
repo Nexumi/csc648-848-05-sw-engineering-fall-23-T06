a57672efcff7a06cef09b645382017c196252e5a
liendavid <dlien@mail.sfsu.edu>
2023-11-27 15:41:56 -1000

    added comment to show token expires in  24 hours

--- application/backend/src/main/java/com/orderowl/api/config/JwtService.java
+++ application/backend/src/main/java/com/orderowl/api/config/JwtService.java
@@ -46,7 +46,7 @@ public class JwtService {
                 .setClaims(extraClaims)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 3600 * 24))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 3600 * 24)) //24hours
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
