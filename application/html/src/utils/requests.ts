0ddb1841451eaea6de0e80dd145375f80de60e50
unknown <jpan6@mail.sfsu.edu>
2023-11-30 23:27:33 -0800

    requests: add axios bearer token

--- application/html/src/utils/requests.ts
+++ application/html/src/utils/requests.ts
@@ -1,7 +1,19 @@
 import axios from 'axios';
 import { apiLogin, apiRegistration, apiTest, apiTracking } from './uri';
+import Cookies from 'js-cookie';
 
 axios.defaults.baseURL = import.meta.env.PROD ? "https://api.orderowl.jpkit.us" : "/";
+axios.interceptors.request.use(
+  config => {
+    const token = Cookies.get('token');
+
+    if (token) {
+      config.headers!['Authorization'] = "Bearer " + token;
+    }
+
+    return config;
+  }
+)
 
 export const getAllTest = async () => {
   try {
