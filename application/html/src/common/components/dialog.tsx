a3c9d19db7585674f4c47f860836e9e2f7098707
mayeung <mankityeung15@gmail.com>
2023-12-01 19:17:47 -0800

    pop up for hidden list needs to connect input to actuall stored pin

--- application/html/src/common/components/dialog.tsx
+++ application/html/src/common/components/dialog.tsx
@@ -1,10 +1,11 @@
 import type { Component, ComponentProps } from "solid-js"
 import { Show, splitProps } from "solid-js"
-
+import { useNavigate} from "@solidjs/router";
 import { Dialog as DialogPrimitive } from "@kobalte/core"
 
 import { Icons } from "../icons"
 import { cn } from "../utils"
+import { uriTracking } from "../../utils/uri";
 
 const Dialog = DialogPrimitive.Root
 
@@ -38,7 +39,11 @@ const DialogOverlay: Component<DialogPrimitive.DialogOverlayProps> = (props) => 
 }
 
 const DialogContent: Component<DialogPrimitive.DialogContentProps> = (props) => {
+  const navigate = useNavigate();
   const [, rest] = splitProps(props, ["class", "children"])
+  const handleCloseClick = () => {
+    navigate(uriTracking()); 
+  };
   return (
     <DialogPortal>
       <DialogOverlay />
@@ -50,16 +55,19 @@ const DialogContent: Component<DialogPrimitive.DialogContentProps> = (props) => 
         {...rest}
       >
         {props.children}
-        <Show when={!props.hideCloseButton}>
-          <DialogPrimitive.CloseButton class="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute right-4 top-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none">
+        {props.hideCloseButton ? null : ( 
+          <DialogPrimitive.CloseButton
+            class="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute right-4 top-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none"
+            onClick={handleCloseClick}
+          >
             <Icons.close class="h-4 w-4" />
             <span class="sr-only">Close</span>
           </DialogPrimitive.CloseButton>
-        </Show>
+        )}
       </DialogPrimitive.Content>
     </DialogPortal>
-  )
-}
+  );
+};
 
 const DialogHeader: Component<ComponentProps<"div">> = (props) => {
   const [, rest] = splitProps(props, ["class"])
--- application/html/src/pages/HiddenTrackingPage.tsx
+++ application/html/src/pages/HiddenTrackingPage.tsx
@@ -7,6 +7,8 @@ import { Flex } from "../common/layout/flex";
 import TrackingList from "../components/TrackingList";
 import { getTrackingBySearch, getTrackingCount } from "../utils/requests";
 import { uriHiddenTracking, uriTracking } from "../utils/uri";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "../common/components/dialog";
+
 
 export default function TrackingPage() {
 
@@ -35,54 +37,82 @@ export default function TrackingPage() {
   });
 
   return (
-    <div class="space-y-4">
-      <span class="text-xl ml-4">Number of Undelivered Orders Left: {undeliveredCount()}</span>
-      <div class="text-center text-6xl">
-        <p>Hidden Track Info List</p>
+    <>
+        <Dialog open>
+          <DialogContent class="bg-orange-50 border-2 border-black" >
+              <DialogHeader class="space-y-4">
+                  <DialogTitle class="text-center text-3xl">
+                      Welcome!
+                  </DialogTitle>
+                  <DialogDescription class="text-center">
+                      Enter your password to access the hidden list!.
+                  </DialogDescription>
+                  <form class="flex flex-col items-center space-y-4">
+                      <input
+                          type="password"
+                          placeholder="Password"
+                          class="w-full p-2 border-2 border-gray-300 rounded"
+                          required
+                      />
+                      <Button
+                          type="submit"
+                          class="text-white bg-black hover:bg-gray-600 w-full"
+                      >
+                          Submit
+                      </Button>
+                  </form>
+              </DialogHeader>
+          </DialogContent>
+        </Dialog>
+      <div class="space-y-4">
+        <span class="text-xl ml-4">Number of Undelivered Orders Left: {undeliveredCount()}</span>
+        <div class="text-center text-6xl">
+          <p>Hidden Track Info List</p>
+        </div>
+        <Flex justifyContent="center">
+          <form use:form>
+            <span class="text-xl mr-2">Search:</span>
+            <input name="search" class="w-64 px-4 py-2 rounded-full text-gray-600 focus:outline-none border focus:border-gray-600" type="text" placeholder="tracking #, carrier, status..." value={searchParams.searchText || ""} />
+            <button type="submit" class="ml-2 px-2 py-2 focus:outline-none rounded-full">
+              <img src={SearchIcon} alt="Search" class="w-6 h-6" />
+            </button>
+          </form>
+          <div>
+            <Flex class="gap-x-2">
+              <Button
+                type="button"
+                class="text-xl mr-2"
+                class={data().listType === "visible" || data().listType === undefined ? IS_TYPE : IS_NOT_TYPE}
+                onClick={() => {
+                  setData("listType", "visible");
+                  navigate(uriTracking());
+                }}
+              >
+                View Visible List
+              </Button>
+              <Button
+                type="button"
+                class="text-xl mr-2"
+                class={data().listType === "hidden" || data().listType === undefined ? IS_TYPE : IS_NOT_TYPE}
+                onClick={() => {
+                  setData("listType", "hidden");
+                  navigate(uriHiddenTracking());
+                }}
+              >
+                View Hidden List
+              </Button>
+            </Flex>
+          </div>
+        </Flex>
+        <Flex justifyContent="center">
+          <div class="w-3/4">
+            <TrackingList
+              display={packages()}
+              sort
+            />
+          </div>
+        </Flex>
       </div>
-      <Flex justifyContent="center">
-        <form use:form>
-          <span class="text-xl mr-2">Search:</span>
-          <input name="search" class="w-64 px-4 py-2 rounded-full text-gray-600 focus:outline-none border focus:border-gray-600" type="text" placeholder="tracking #, carrier, status..." value={searchParams.searchText || ""} />
-          <button type="submit" class="ml-2 px-2 py-2 focus:outline-none rounded-full">
-            <img src={SearchIcon} alt="Search" class="w-6 h-6" />
-          </button>
-        </form>
-        <div>
-          <Flex class="gap-x-2">
-            <Button
-              type="button"
-              class="text-xl mr-2"
-              class={data().listType === "visible" || data().listType === undefined ? IS_TYPE : IS_NOT_TYPE}
-              onClick={() => {
-                setData("listType", "visible");
-                navigate(uriTracking());
-              }}
-            >
-              View Visible List
-            </Button>
-            <Button
-              type="button"
-              class="text-xl mr-2"
-              class={data().listType === "hidden" || data().listType === undefined ? IS_TYPE : IS_NOT_TYPE}
-              onClick={() => {
-                setData("listType", "hidden");
-                navigate(uriHiddenTracking());
-              }}
-            >
-              View Hidden List
-            </Button>
-          </Flex>
-        </div>
-      </Flex>
-      <Flex justifyContent="center">
-        <div class="w-3/4">
-          <TrackingList
-            display={packages()}
-            sort
-          />
-        </div>
-      </Flex>
-    </div>
+    </>
   );
 }
